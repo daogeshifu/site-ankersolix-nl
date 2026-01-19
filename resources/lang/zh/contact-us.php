e8c8ec6a60dca34b8d05c696defb686bb09e173b
EasyToFind1 <719281404@qq.com>
2026-01-19 13:07:51 +0800

    多语言

---
+++ resources/lang/zh/contact-us.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'contact_title' => '联系我们',
+    'contact_description' => '如有问题、反馈或合作意向，欢迎与我们联系。',
+    'contact_keywords' => '联系, 我们, 问题, 反馈, 合作',
+
+    'title' => '联系我们',
+    'description' => '如有问题、反馈或合作意向，欢迎与我们联系。',
+
+    'form_title' => '给我们留言',
+    'name' => '您的姓名',
+    'email' => '电子邮箱',
+    'subject' => '主题',
+    'message' => '留言内容',
+    'submit' => '发送消息',
+
+    'info_title' => '联系信息',
+    'response_time' => '回复时间',
+    'response_desc' => '我们通常会在 24–48 小时内回复。',
+    'privacy_notice' => '您的信息仅用于回复您的咨询。',
+];
