87d8a412fbd9437f77958afa2da5f6e092ba7570
lijiajin <jiajin.li@winndoo.com>
2026-05-09 13:28:22 +0800

    remove nl

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'super' => 'bestenthuisbatterij.nl',
+    'super' => 'bestenthuisbatterij',
     'brand' => 'bestenthuisbatterij.nl - Home battery insights and guides for Dutch households',
     'blog_title' => 'Insights & Guides',
     'blog_description' => 'Practical updates on home batteries, energy storage, solar self-consumption, dynamic tariffs, and subsidy opportunities for households in the Netherlands.',
--- resources/lang/nl/home.php
+++ resources/lang/nl/home.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'super' => 'bestenthuisbatterij.nl',
+    'super' => 'bestenthuisbatterij',
     'brand' => 'bestenthuisbatterij.nl - inzichten en gidsen over thuisbatterijen voor Nederlandse huishoudens',
     'blog_title' => 'Inzichten & gidsen',
     'blog_description' => 'Praktische updates over thuisbatterijen, energieopslag, eigen verbruik van zonne-energie, dynamische tarieven en subsidiemogelijkheden in Nederland.',
